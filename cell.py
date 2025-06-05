12f3601e856ba49cc8a32317b16abfbdc071a2b8
Trev-D-Dev <dunn.trevor@pm.me>
2025-06-05 19:59:56 -0400

    Created Cell class, properly draws cells

---
+++ cell.py
@@ -0,0 +1,60 @@
+from window import Window
+from point import Point
+from line import Line
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = window
+    
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        if(self.has_left_wall):
+            top_x = self.__x1
+            top_y = self.__y1
+            bottom_x = self.__x1
+            bottom_y = self.__y2
+            top_point = Point(top_x, top_y)
+            bottom_point = Point(bottom_x, bottom_y)
+            left_line = Line(top_point, bottom_point)
+            self.__win.draw_line(left_line, "black")
+
+        if(self.has_right_wall):
+            top_x = self.__x2
+            top_y = self.__y1
+            bottom_x = self.__x2
+            bottom_y = self.__y2
+            top_point = Point(top_x, top_y)
+            bottom_point = Point(bottom_x, bottom_y)
+            right_line = Line(top_point, bottom_point)
+            self.__win.draw_line(right_line, "black")
+
+        if(self.has_top_wall):
+            left_x = self.__x1
+            left_y = self.__y1
+            right_x = self.__x2
+            right_y = self.__y1
+            left_point = Point(left_x, left_y)
+            right_point = Point(right_x, right_y)
+            top_line = Line(left_point, right_point)
+            self.__win.draw_line(top_line, "black")
+
+        if(self.has_bottom_wall):
+            left_x = self.__x1
+            left_y = self.__y2
+            right_x = self.__x2
+            right_y = self.__y2
+            left_point = Point(left_x, left_y)
+            right_point = Point(right_x, right_y)
+            bottom_line = Line(left_point, right_point)
+            self.__win.draw_line(bottom_line, "black")
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from window import Window
 from point import Point
 from line import Line
+from cell import Cell
 
 def main():
     win = Window(800, 800)
 
-    point_a = Point(5, 10)
-    point_b = Point(30, 40)
-    line_a = Line(point_a, point_b)
-    win.draw_line(line_a, "red")
+    cell_1 = Cell(win)
+    cell_2 = Cell(win)
+    cell_3 = Cell(win)
+    cell_4 = Cell(win)
+
+    cell_1.draw(20, 20, 40, 40)
+    cell_2.draw(40, 20, 60, 40)
+    cell_3.draw(60, 20, 80, 40)
+    cell_4.draw(80, 20, 100, 40)
 
     win.wait_for_close()
 
