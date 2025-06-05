0e87eb119704965e9ecc73960944ad81ef24afa8
Trev-D-Dev <dunn.trevor@pm.me>
2025-06-05 19:42:26 -0400

    Finished point and line classes, got lines to be drawn on canvas

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 800)
+
+    point_a = Point(5, 10)
+    point_b = Point(30, 40)
+    line_a = Line(point_a, point_b)
+    win.draw_line(line_a, "red")
+
     win.wait_for_close()
 
 main()
